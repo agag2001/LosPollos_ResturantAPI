ac9c6add3893807f63719e2b1e30ef7ac1a60218
abdallah agag <bodyagag@gmail.com>
2025-02-09 04:00:05 +0200

    Add project files.

---
+++ LosPollos.Domain/Entities/Resturant.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LosPollos.Domain.Entities
+{
+    public class Resturant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;    
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        
+        public bool HasDelivery { get; set; }       
+
+        public string? CantactEmail { get; set; }   
+        public string? CantactPhone { get; set; }
+        public Address? Address { get; set; }
+
+        public List<Dish> Dishes { get; set; } = new();
+    }
+}
